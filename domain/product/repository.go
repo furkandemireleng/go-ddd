b46ff3299d9b5d2f1c3651af7f5cda1cdaef759d
Furkan Demirel <furkandemireleng@gmail.com>
2024-05-12 00:33:36 +0300

    Product repository completed

--- domain/product/repository.go
+++ domain/product/repository.go
@@ -1,1 +1,14 @@
 package product
+
+import (
+	"github.com/furkandemireleng/go-ddd/aggregate"
+	"github.com/google/uuid"
+)
+
+type ProductRepository interface {
+	GetAll() ([]aggregate.Product, error)
+	GetByID(id uuid.UUID) (aggregate.Product, error)
+	Add(p aggregate.Product) (aggregate.Product, error)
+	Update(p aggregate.Product) (aggregate.Product, error)
+	Delete(id uuid.UUID) error
+}
